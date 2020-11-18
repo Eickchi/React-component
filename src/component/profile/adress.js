01adaf92074f715134010bf9d7f2fe1cef32ba9c
Eickchi <ahmedbouabda22@gmail.com>
2020-11-18 22:01:26 +0100

    Components

---
+++ src/component/profile/adress.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typing from 'react-typing-animation';
+
+const adress = () => {
+    return (
+        <div>
+            <Typing>
+            <h1>ahmedbouabda22@gmail.com</h1> 
+            </Typing>
+        </div>
+    )
+}
+
+export default adress
